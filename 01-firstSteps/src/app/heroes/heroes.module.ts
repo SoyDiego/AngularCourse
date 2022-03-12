35b4c98b720270d794d2734c5050975ec3b6a9a2
soydiego <diegofranchina@gmail.com>
2022-03-12 18:48:26 +0100

    modules and end section

---
+++ 01-firstSteps/src/app/heroes/heroes.module.ts
@@ -0,0 +1,11 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { HeroeComponent } from './heroe/heroe.component';
+import { ListComponent } from './list/list.component';
+
+@NgModule({
+  declarations: [HeroeComponent, ListComponent],
+  imports: [CommonModule],
+  exports: [ListComponent],
+})
+export class HeroesModule {}
