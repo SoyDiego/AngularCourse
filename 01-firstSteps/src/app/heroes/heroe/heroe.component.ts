dc0c56a7a8fdf7250800867f56320e1cfa9a0e42
soydiego <diegofranchina@gmail.com>
2022-03-12 11:57:27 +0100

    Template changes

--- 01-firstSteps/src/app/heroes/heroe/heroe.component.ts
+++ 01-firstSteps/src/app/heroes/heroe/heroe.component.ts
@@ -3,6 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-heroe',
   templateUrl: './heroe.component.html',
-  styleUrls: ['./app.component.css'],
+  // styleUrls: ['./app.component.css'],
 })
-export class HeroeComponent {}
+export class HeroeComponent {
+  name: string = 'ironman';
+  age: number = 45;
+
+  get capitalizeName(): string {
+    return this.name.toUpperCase();
+  }
+
+  getName(): string {
+    return `${this.name} is ${this.age} years old`;
+  }
+}
