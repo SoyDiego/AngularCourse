a6920387da18b7a3cbcdd071daa340278ead2bfb
soydiego <diegofranchina@gmail.com>
2022-03-13 18:13:10 +0100

    Showing characters with ngFor, used ngModel in form

--- 01-firstSteps/src/app/dbz/main-page/main-page.component.ts
+++ 01-firstSteps/src/app/dbz/main-page/main-page.component.ts
@@ -10,16 +10,28 @@ interface Character {
   templateUrl: './main-page.component.html',
 })
 export class MainPageComponent {
+  characters: Character[] = [
+    { name: 'Goku', power: 9000 },
+    { name: 'Vegeta', power: 8000 },
+    { name: 'Piccolo', power: 7000 },
+    { name: 'Gohan', power: 6000 },
+  ];
+
   new: Character = {
-    name: 'Trunks',
-    power: 14000,
+    name: '',
+    power: 0,
   };
 
   addCharacter() {
-    console.log('Adding new character:', this.new);
+    if (this.new.name.trim().length === 0) {
+      return;
+    }
+
+    this.characters.push(this.new);
+    this.new = {
+      name: '',
+      power: 0,
+    };
   }
 
-  changeName(name: any) {
-    this.new.name = name;
-  }
 }
