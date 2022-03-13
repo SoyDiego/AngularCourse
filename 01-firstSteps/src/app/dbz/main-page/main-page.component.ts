fb776a2fd7f349b8575aa3fdf2ccb8b026b23b9b
soydiego <diegofranchina@gmail.com>
2022-03-13 17:59:34 +0100

    ngModel in forms - two ways data-binding

--- 01-firstSteps/src/app/dbz/main-page/main-page.component.ts
+++ 01-firstSteps/src/app/dbz/main-page/main-page.component.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
 
+interface Character {
+  name: string;
+  power: number;
+}
+
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
 })
 export class MainPageComponent {
+  new: Character = {
+    name: 'Trunks',
+    power: 14000,
+  };
+
   addCharacter() {
-    console.log('add character');
+    console.log('Adding new character:', this.new);
+  }
+
+  changeName(name: any) {
+    this.new.name = name;
   }
 }
