4cf4510e424af945c443af73dd5800d7c2f88a07
soydiego <diegofranchina@gmail.com>
2022-03-16 20:15:02 +0100

    created child components

---
+++ 01-firstSteps/src/app/dbz/characters/characters.component.ts
@@ -0,0 +1,9 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-characters',
+  templateUrl: './characters.component.html',
+})
+export class CharactersComponent {
+  characters: any[] = [];
+}
--- 01-firstSteps/src/app/dbz/dbz.module.ts
+++ 01-firstSteps/src/app/dbz/dbz.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainPageComponent } from './main-page/main-page.component';
 import { FormsModule } from '@angular/forms';
+import { CharactersComponent } from './characters/characters.component';
 
 @NgModule({
-  declarations: [MainPageComponent],
+  declarations: [MainPageComponent, CharactersComponent],
   imports: [CommonModule, FormsModule],
   exports: [MainPageComponent],
 })
