c48d17f9e7a810addcb98839da0433d51cd2e0ce
soydiego <diegofranchina@gmail.com>
2022-03-17 19:31:42 +0100

    Centralize access to characters with Services

--- 01-firstSteps/src/app/dbz/main-page/main-page.component.ts
+++ 01-firstSteps/src/app/dbz/main-page/main-page.component.ts
@@ -1,29 +1,18 @@
 import { Component } from '@angular/core';
 import { Character } from '../interfaces/dbz.interfaces';
-import { DbzService } from '../services/dbz.service';
 
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
 })
 export class MainPageComponent {
-  characters: Character[] = [
-    { name: 'Goku', power: 9000 },
-    { name: 'Vegeta', power: 8000 },
-    { name: 'Piccolo', power: 7000 },
-    { name: 'Gohan', power: 6000 },
-  ];
-
+  
   new: Character = {
     name: '',
     power: 0,
   };
 
-  addNewCharacter(character: Character): void {
-    this.characters.push(character)
-  }
-
-  constructor(private dbzService: DbzService) {
+  constructor() {
     
   }
 }
--- 01-firstSteps/src/app/dbz/services/dbz.service.ts
+++ 01-firstSteps/src/app/dbz/services/dbz.service.ts
@@ -1,7 +1,18 @@
 import { Injectable } from '@angular/core';
+import { Character } from '../interfaces/dbz.interfaces';
 
 @Injectable()
 export class DbzService {
+  private _characters: Character[] = [
+    { name: 'Goku', power: 9000 },
+    { name: 'Vegeta', power: 8000 },
+    { name: 'Piccolo', power: 7000 },
+    { name: 'Gohan', power: 6000 },
+  ];
+
+  get characters(): Character[] {
+    return [...this._characters];
+  }
   constructor() {
     console.log('DbzService initialized');
   }
