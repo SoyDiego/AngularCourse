caf59c6c48ace278b8d3a7e812b1917ae27f3527
soydiego <diegofranchina@gmail.com>
2022-04-11 20:24:21 +0200

    Created modules and basic components

--- 03-countriesApp/src/app/app.module.ts
+++ 03-countriesApp/src/app/app.module.ts
@@ -2,15 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CountriesModule } from './countries/countries.module';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CountriesModule,
+    SharedModule
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
