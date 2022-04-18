a1a9fccd133167a41b89683b33bcbe9a298dd658
soydiego <diegofranchina@gmail.com>
2022-04-18 19:48:53 +0200

    Router Module: routes in the app

--- 03-countriesApp/src/app/app.module.ts
+++ 03-countriesApp/src/app/app.module.ts
@@ -1,19 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { CountriesModule } from './countries/countries.module';
 import { SharedModule } from './shared/shared.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    CountriesModule,
-    SharedModule
-  ],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, CountriesModule, SharedModule],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
