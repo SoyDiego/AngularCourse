06af9e141a435b367b08cccaa24a4551cb8cb40f
Diego Franchina <diego.franchina@convidad.es>
2022-04-20 15:26:26 +0200

    Created sidebar component and showed

--- 03-countriesApp/src/app/shared/sidebar/sidebar.component.ts
+++ 03-countriesApp/src/app/shared/sidebar/sidebar.component.ts
@@ -3,6 +3,12 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
+  styles: [
+    `
+    li{
+      cursor: pointer;
+    }`
+  ]
 })
 export class SidebarComponent implements OnInit {
 
