a1e41cb5e52ced81dedfcd5f0a358194b1cde7a8
Diego Franchina <diego.franchina@convidad.es>
2022-04-22 13:09:28 +0200

    Created/separated Input and Table components

---
+++ 03-countriesApp/src/app/countries/components/country-input/country-input.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-country-input',
+  templateUrl: './country-input.component.html',
+  styles: [
+  ]
+})
+export class CountryInputComponent {
+
+  search: string = '';
+
+  searchCountry(){
+    
+  }
+
+
+
+}
