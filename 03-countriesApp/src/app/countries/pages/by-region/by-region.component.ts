baf0870bfe092ffe34de842e6329d33528f736c4
soydiego <diegofranchina@gmail.com>
2022-05-08 11:37:01 +0200

    Conditional CSS Classes

--- 03-countriesApp/src/app/countries/pages/by-region/by-region.component.ts
+++ 03-countriesApp/src/app/countries/pages/by-region/by-region.component.ts
@@ -1,16 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-by-region',
   templateUrl: './by-region.component.html',
-  styles: [
-  ]
 })
-export class ByRegionComponent implements OnInit {
+export class ByRegionComponent {
+  regions: string[] = ['africa', 'americas', 'asia', 'europe', 'oceania'];
+  activeRegion: string = '';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  activateRegion(region: string) {
+    this.activeRegion = region;
   }
 
+  getCSSClass(region: string): string {
+    return region === this.activeRegion
+      ? 'btn btn-primary me-2'
+      : 'btn btn-outline-primary me-2';
+  }
 }
