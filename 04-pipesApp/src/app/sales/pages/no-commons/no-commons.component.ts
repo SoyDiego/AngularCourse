4753e2341f8eb3f6a2ac90712589ae321dfc77a0
Diego Franchina <diego.franchina@convidad.es>
2022-07-29 12:52:55 +0200

    i18nSelect & i18nPlural

--- 04-pipesApp/src/app/sales/pages/no-commons/no-commons.component.ts
+++ 04-pipesApp/src/app/sales/pages/no-commons/no-commons.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-no-commons',
@@ -6,11 +6,24 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class NoCommonsComponent implements OnInit {
+export class NoCommonsComponent {
 
-  constructor() { }
+  name: string = 'Susana';
+  genre: string = 'female';
 
-  ngOnInit(): void {
+  // i18nSelect
+  invitationMap = {
+    'male': 'invitarlo',
+    'female': 'invitarla'
+  }
+
+  // i18nPlural
+  clients: string[] = ['María', 'Juan', 'Pedro', 'Ricardo', 'Paco']
+  clientsMap = {
+    '=0': 'no tenemos ningún cliente esperando.',
+    '=1': 'tenemos un cliente esperando.',
+    'other': 'tenemos # clientes esperando.'
+
   }
 
 }
